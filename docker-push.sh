16de8a72e36ab4b9c2873f742d6b461fe9b9c124
Damien BUTY <dam.buty@gmail.com>
2025-09-09 23:44:10 +0200

    🐙 Github actions

--- docker-push.sh
+++ docker-push.sh
@@ -5,12 +5,6 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | grep -v '^$' | xargs)
 fi
 
-echo VITE_TMDB_API_KEY="$VITE_TMDB_API_KEY"
-echo VITE_API_URL="$VITE_API_URL"
-
-echo "$DOCKERHUB_USERNAME"
-echo "$DOCKERHUB_TOKEN"
-
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 docker push dambuty/lazone-videotek:latest
