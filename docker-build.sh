19856c67b556c7f65e0a4d36a3f38e3b2bf33bc2
Damien BUTY <dam.buty@gmail.com>
2025-09-09 23:52:30 +0200

    🐙 Github actions

--- docker-build.sh
+++ docker-build.sh
@@ -5,9 +5,6 @@ if [ -f .env ]; then
   export $(grep -v '^#' .env | grep -v '^$' | xargs)
 fi
 
-echo VITE_TMDB_API_KEY="$VITE_TMDB_API_KEY"
-echo VITE_API_URL="$VITE_API_URL"
-
 # Build & tag the image
 docker build \
   -f Dockerfile \
